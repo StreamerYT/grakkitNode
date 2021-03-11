7be35333e7b257b623c3947fc0a17fb977bd0917
StreamerYT <41978062+StreamerYT@users.noreply.github.com>
2021-03-11 10:41:08 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-let server = require('./grakkit')
-server.initModule('require')
-server.on('ready',()=>server.require('./indexg.js'))
+let grakkit = require('./grakkit')
+let server = grakkit('./server/','craftbukkit-1.12.2.jar')
+
+server.on('console',l=>console.log(l))
+process.stdin.on('data',d=>server.jar.stdin.write(d))
+
+server.autorestart=1
+server.start()
