257c1aa76768246ce963d6ed48b45167aa60a59e
StreamerYT <41978062+StreamerYT@users.noreply.github.com>
2021-03-11 10:42:23 +1000

    Update index.js

--- grakkit/index.js
+++ grakkit/index.js
@@ -1,56 +1,77 @@
-let {spawn} = require('child_process')
-let fs = require('fs')
+let node = globalThis.process
 
-global.server = {}
-
-server._events = {}
-server.on=function (name,func){
-    if(!server._events[name])server._events[name]=[]
-    server._events[name].push(func)
-}
-server._emit = function (name,...args){
-    if(server._events[name])server._events[name].forEach(f=>f(...args))
-}
-server.emit = function (name,...args){
-    server.eval(`server._emit('${name}',JSON.parse(\`${JSON.stringify(args)}\`))`)
-    if(server._events[name])server._events[name].forEach(f=>f(...args))
+function globalFunctions(server){
+    server._events = {}
+    server.on=(n,f)=>{if(!server._events[n])server._events[n]={on:[],once:[]};server._events[n].on.push(f)}
+    server.once=(n,f)=>{if(!server._events[n])server._events[n]={on:[],once:[]};server._events[n].once.push(f)}
+    server.__emitonce=(n,f=server._events[n].once.splice(0,1)[0],...a)=>{if(f){f(...a);server.__emitonce(n,undefined,...a)}}
+    server._emit=(n,...a)=>{if(server._events[n]){server._events[n].on.forEach(f=>f(...a));server.__emitonce(n,undefined,...a)}}
+    server.emit=(n,...a)=>{node?server.eval(`global.server._emit('${n}',...JSON.parse(\`${JSON.stringify(a)}\`))`):server._send('event',{n,a});server._emit(n,...a)}
+    if(node)server.ondata.event=d=>server._emit(d.n,...d.a)
 }
 
-server.eval = s=>jar.stdin.write(`eval ${s}\n`)
-
-server.ondata={}
-server.initModule=(n,...d)=>{let m=require('./modules/'+n.toLowerCase()+'.js');try{m(...d)}catch{}}
-server.ondata.event = a=>server._emit(a.name,...a.args)
-
-function log(data){
-    data=data.toString()
-    ' '.repeat(2).split(' ').forEach(()=>{
-        if(data.endsWith('\n')) data=data.substring(0,data.length-1)
-    })
-    let d = data.split('INFO]: ')[1]
-        d = d?d.split(' ')[0]:null
-    if(d&&d=="_grakkit@node_"){
-        data = JSON.parse(data.split('INFO]: ')[1].split(' ').slice(1).join(" "))
-        if(server.ondata[data.name])server.ondata[data.name](data.data)
-    }else{
-        server._emit('console',data)
+if(node){
+    global.path = require('path')
+    let {spawn} = require('child_process')
+    function log(server,data){data=data.toString()
+        ' '.repeat(2).split(' ').forEach(()=>data.endsWith('\n')?data=data.substring(0,data.length-1):0)
+        let d=data.split('INFO]: ')[1];d=d?d.split(' ')[0]:0
+        if(d&&d=="_grakkit@node_"){
+            d = JSON.parse(data.split('INFO]: ')[1].split(' ').slice(1).join(" "))
+            if(server.ondata[d.n])server.ondata[d.n](d.d)
+        }else{
+            server._emit('console',data)
+        }
     }
+    let offlinejar = {stdin:{write:()=>{console.warn(new Error("You haven't started the server yet!"))}}}
+
+    module.exports = function (cwd,file){
+        let server = {_ready:0,autorestart:0,ondata:[],cwd:path.join(__dirname,'../',cwd),file,jar:offlinejar}
+        globalFunctions(server)
+
+        server.on('ready',()=>server._ready=1)
+        server.on('stop',()=>{
+            server.ready=0;
+            server.jar=offlinejar
+            if(server.autorestart){setTimeout(()=>server.start(),1000)}
+        })
+        
+        server.execute = x=>server.jar.stdin.write(`${x}\n`)
+        server.eval = x=>server.execute(`eval ${x}`)
+        server.module = (name,...args)=>require('./modules/'+name+'.js')(server,...args)
+
+        server.start = function (){
+            if(server.jar!=offlinejar) return console.log(new warn('You have already started the server!'))
+            server.jar = spawn('java',`-jar ${file}`.split(' '),{cwd:server.cwd})
+            server.jar.stdout.on('data',l=>log(server,l));
+            server.jar.stderr.on('data',l=>log(server,l));
+            server.jar.on('close',c=>server._emit('stop',c));
+        }
+        server.stop = function (){
+            if(server._ready){
+                server.execute('stop')
+            }else{
+                console.log("You haven't started the server yet! But I still try to turn it off.")
+                if(server.jar==offlinejar){
+                    console.warn(new Error("You haven't started the server yet!"))
+                }else{
+                    server.jar.kill()
+                }
+            }
+        }
+
+        return server
+    }
+
+}else{
+    globalThis.global=globalThis
+    global.server = {
+        _send:(n,d)=>console.log(`_grakkit@node_ ${JSON.stringify({n,d})}`)
+    }
+    globalFunctions(global.server)
+    let b = Java.type('org.bukkit.Bukkit')
+    let g = b.getPluginManager().getPlugin('grakkit');
+    let c = b.getConsoleSender()
+    g.register(g.getName(),'eval',[],'0','0',(s,n,a)=>s==c?eval([...a].join(' ')):0,s=>0)
+    server.emit('ready')
 }
-
-let jar = {stdin:{write:()=>{throw Error("You haven't started the server yet!")}}}
-
-server.start = ()=>{
-    server.start=()=>{}
-    jar = spawn('java',`-jar ${fs.readdirSync('./server').find(f=>f.endsWith('.jar'))}`.split(' '),{cwd:"./server"})
-    jar.on('close',c=>process.exit(c));
-    jar.stdout.on('data',log);
-    jar.stderr.on('data',log);
-    process.stdin.on('data',d=>{
-        server.emit('console','>'+d.toString())
-        jar.stdin.write(d)
-    })
-}
-
-
-
-module.exports = global.server
