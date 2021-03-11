b08722024320c76a81e2b83f6d736849ef141520
StreamerYT <41978062+StreamerYT@users.noreply.github.com>
2021-03-11 10:47:33 +1000

    Update function.js

--- grakkit/modules/function.js
+++ grakkit/modules/function.js
@@ -1,12 +1,19 @@
-let node = !!globalThis.process 
-
-if(node){
-    server.initModule('require')
-    server.require('./grakkit/modules/function.js')
-    server.function = function (f,...args){
-        server.emit('_grakkit@node_&function_',f.toString().replace(/\n/g,'_grakkit@node_&newLine_'),...args)
+if(!!global.process){
+    module.exports = function (server){
+        if(!server.require)server.module('require')
+        server.require('./grakkit/modules/function.js',1)
+        server.function = function (func,autoRequireOnRestart,...args){
+            let r = ()=>server.emit('_grakkit@node_&function',func.toString().replace(/\n/g,'_grakkit@node_&newLine'),...args)
+            if(server.ready){
+                r()
+                if(autoRequireOnRestart) return server.on('ready',()=>{r()})
+                delete r
+            }else{
+                server[autoRequireOnRestart?'on':'once']('ready',()=>{r();if(!autoRequireOnRestart)delete r;})
+            }
+        }
     }
 }else{
-    let rg = new RegExp('_grakkit@node_&newLine_','g')
-    server.on('_grakkit@node_&function_',(f,...a)=>eval(f.replace(rg,'\n'))(...a))
+    let rg = new RegExp('_grakkit@node_&newLine','g')
+    server.on('_grakkit@node_&function',(f,...a)=>{eval(f.replace(rg,'\n'))(...a)})
 }
