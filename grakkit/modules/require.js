45b684a6f621a3cc94109dacd8a5304dd5ec9d4b
StreamerYT <41978062+StreamerYT@users.noreply.github.com>
2021-03-11 10:47:59 +1000

    Update require.js

--- grakkit/modules/require.js
+++ grakkit/modules/require.js
@@ -1,2 +1,12 @@
-let path = require('path');
-server.require=p=>server.eval(`require('${path.join(path.join(__dirname,'../../'),p)}')`)
+module.exports = function (server){
+    server.require=(module,autoRequireOnRestart)=>{
+        let r = ()=>server.eval(`require('${path.join(__dirname,'../../',module)}')`)
+        if(server.ready){
+            r()
+            if(autoRequireOnRestart) return server.on('ready',()=>{r()})
+            delete r
+        }else{
+            server[autoRequireOnRestart?'on':'once']('ready',()=>{r();if(!autoRequireOnRestart)delete r;})
+        }
+    }
+}
